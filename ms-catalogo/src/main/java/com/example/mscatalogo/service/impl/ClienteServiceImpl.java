ffa2275b4bfae74cfee25974d447457d3e877ac9
flormery <meryelizabeth19@gmail.com>
2024-04-04 10:24:51 -0500

    desarollo

---
+++ ms-catalogo/src/main/java/com/example/mscatalogo/service/impl/ClienteServiceImpl.java
@@ -0,0 +1,41 @@
+package com.example.mscatalogo.service.impl;
+
+import com.example.mscatalogo.entity.Cliente;
+import com.example.mscatalogo.repository.ClienteRepository;
+import com.example.mscatalogo.service.ClienteService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Service;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@Service
+public class ClienteServiceImpl implements ClienteService {
+    @Autowired
+    private ClienteRepository clienteRepository;
+    @Override
+    public List<Cliente> listar() {
+        return clienteRepository.findAll();
+    }
+
+    @Override
+    public Cliente guardar(Cliente cliente) {
+        return clienteRepository.save(cliente);
+    }
+
+    @Override
+    public Cliente buscarPorId(Integer id) {
+        return clienteRepository.findById(id).get();
+    }
+
+    @Override
+    public Cliente editar(Cliente cliente) {
+        return clienteRepository.save(cliente);
+    }
+
+    @Override
+    public void eliminar(Integer id) {
+        clienteRepository.deleteById(id);
+    }
+}
